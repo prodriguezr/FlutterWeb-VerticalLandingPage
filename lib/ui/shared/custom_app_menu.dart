b0dba195fa5e18daf8cfc3e82b6c2da1c37db7ed
Pablo Rodríguez <prodrigu75@gmail.com>
2021-08-21 17:43:30 -0400

    Added basics features to the menu

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -6,14 +6,35 @@ class CustomAppMenu extends StatefulWidget {
   _CustomAppMenuState createState() => _CustomAppMenuState();
 }
 
-class _CustomAppMenuState extends State<CustomAppMenu> {
+class _CustomAppMenuState extends State<CustomAppMenu>
+    with SingleTickerProviderStateMixin {
+  bool isOpen = false;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
         onTap: () {
-          print('Click');
+          if (isOpen) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+
+          setState(() {
+            isOpen = !isOpen;
+          });
         },
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 10),
@@ -22,6 +43,11 @@ class _CustomAppMenuState extends State<CustomAppMenu> {
           color: Colors.black,
           child: Row(
             children: [
+              AnimatedContainer(
+                duration: Duration(milliseconds: 400),
+                curve: Curves.easeInOut,
+                width: isOpen ? 50 : 0,
+              ),
               Text(
                 'Menu',
                 style: GoogleFonts.roboto(
@@ -30,8 +56,9 @@ class _CustomAppMenuState extends State<CustomAppMenu> {
                 ),
               ),
               Spacer(),
-              Icon(
-                Icons.menu,
+              AnimatedIcon(
+                icon: AnimatedIcons.menu_close,
+                progress: controller,
                 color: Colors.white,
               ),
             ],
