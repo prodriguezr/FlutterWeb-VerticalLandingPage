e8fdd39604ef5bb6861773e60d01b407847d6bc3
Pablo Rodríguez <prodrigu75@gmail.com>
2021-08-21 13:02:38 -0400

    Added "home_page" and changed some features of the app in "main.dart"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/pages/pages.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,1 @@
+export 'package:vertical_landing_page/ui/pages/home_page.dart';
